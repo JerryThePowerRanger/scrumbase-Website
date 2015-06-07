2df547ffbc0f34d4c096ba284b9902ba8d591283
Jakob Bal <baljakob9@gmail.com>
2015-06-08 01:09:29 +0200

    Hinzufügen all der Animationsdateien. Die Animation befindet sich in intro.html und ist über den dritten Eintrag im Inhaltsverzeichnis erreichbar.
    Angular Änderungen:
    	– In controller.js wurde eine weitere Methode programmiert, um den Text für den jeweiligen Teil der Animation

--- controller.js
+++ controller.js
@@ -114,14 +114,25 @@ app.controller("TemplateController", function($scope) {
 	
 });
 
-app.controller("AnimationController", function($scope, animationDesc){
+app.controller("AnimationController", function($scope, animationDesc, $log){
+	// Aufrufen des Datenarrays aus dem animationDesc Service
 	var desc = animationDesc.text;
 
-	$scope.showText = function(type){
+	/*
+	*	Diese Methode dient dazu den korrekten Text für die Animation anzuzeigen. Die einzelnen Elemente in der Animation haben einen
+	*	Click–Listener und rufen diese Methode auf, wenn sie gedrückt werden
+	*/
+	$scope.showDescText = function(type){
+		$log.log('hello');
+		$log.log(type);
 		angular.forEach(desc, function(value, key){
 			if(type === value.name){
 				$scope.text = value.text;
 			}
 		});
+	};
+
+	$scope.test = function(){
+		$scope.text = 'Hello';
 	};
 });
--- services.js
+++ services.js
@@ -2,16 +2,11 @@ app.service('animationDesc', [function () {
 	this.text = [
 	{
 		name: 'pb',
-		text: 'Product Backlog: Auflistung aller Anforderungen an das Produkt in Form von User Stories. 
-			   Ersteller: Product Owner
-			   Anmerkung: Es wird laufend vom Product Owner gepflegt und weitergef&uuml;hrt.'
+		text: 'Product Backlog: Auflistung aller Anforderungen an das Produkt in Form von User Stories. Ersteller: Product Owner Anmerkung: Es wird laufend vom Product Owner gepflegt und weitergef&uuml;hrt.'
 	},
 	{
 		name: 'sb',
-		text: 'Sprint Backlog:
-				In ihm stehen alle Anforderungen, die in einem Sprint erledigt werden mu&uuml;ssen in Form von Tasks.(User Stories werden in Tasks unterbrochen).
-				Die zubearbeitenden User Stories werden aus dem Product Backlog entnommen, wobei die einzelnen Tasks priorisiert sind.
-				Der Sprint Backlog wird im Sprint Planning Meeting am Anfang jedes Sprints erstellt.'
+		text: 'Sprint BacklogIn ihm stehen alle Anforderungen, die in einem Sprint erledigt werden mu&uuml;ssen in Form von Tasks.(User Stories werden in Tasks unterbrochen).Die zubearbeitenden User Stories werden aus dem Product Backlog entnommen, wobei die einzelnen Tasks priorisiert sind.Der Sprint Backlog wird im Sprint Planning Meeting am Anfang jedes Sprints erstellt.'
 	},
 	{
 		name: 'sprint',
@@ -19,28 +14,13 @@ app.service('animationDesc', [function () {
 	},
 	{
 		name: 'ds',
-		text: 'Daily Scrum Meeting: Ist ein t&auml;gliches Status-Meeting mit einer Dauer von h&ouml;chstens 10 Minuten f&ouml;r das ganze Team.
-				U.a zu behandelte Fragen:
-				Was wurde das letzte mal erreicht?
-				Wo gab es Probleme?
-				Was soll dieses mal erreicht werden?
-				Dauer: ~10 Minuten'
+		text: 'Daily Scrum Meeting: Ist ein t&auml;gliches Status-Meeting mit einer Dauer von h&ouml;chstens 10 Minuten f&ouml;r das ganze Team.U.a zu behandelte Fragen:Was wurde das letzte mal erreicht?Wo gab es Probleme?Was soll dieses mal erreicht werden?Dauer: ~10 Minuten'
 	},
 	{
 		name: 'sr',
-		text: 'Sprint Review Meeting: Das Team zeigt dem Product Owner was es geschafft hat.
-				Zweck: Fertigstellungsgrad der Feature/ User Stories festzustellen und um Feedback von Anwesenden einzuholen.
-				Durchf&uuml;hrung: Anhand des Sprintbacklogs, werden die einzelne User Stories mit den Acceptance Criteria und Definition of Done Kriterien auf Vollständigkeit und weitere geforderte Eigenschaften begutachtet und bewertet.
-				Anschließend wird beschlossen, ob ein User Story als „Abgenommen“ gilt oder nicht.
-				Dauer pro Mitarbeiter: ~1 Stunde
-				Sprint Retrospective Meeting: Reflektion des letzten Sprints.
-				Ziel: Verbesserungspotential f&uuml;r die zuk&uuml;nftigen Sprints zu finden. Interne Projektprozesse und Spielregeln kritisch zu betrachten.
-				Ablauf: Am Ende eines Sprints setzt sich das Team zusammen und bespricht was im vergangenem Sprint gut bzw. schlecht gelaufen ist.
-				Aufzeichnungen: Ein Protokoll wird erstellt.
-				Dauer: ~1/2h'
-	},
+		text: 'Sprint Review Meeting: Das Team zeigt dem Product Owner was es geschafft hat.Zweck: Fertigstellungsgrad der Feature/ User Stories festzustellen und um Feedback von Anwesenden einzuholen.Durchf&uuml;hrung: Anhand des Sprintbacklogs, werden die einzelne User Stories mit den Acceptance Criteria und Definition of Done Kriterien auf Vollständigkeit und weitere geforderte Eigenschaften begutachtet und bewertet.Anschließend wird beschlossen, ob ein User Story als „Abgenommen“ gilt oder nicht.Dauer pro Mitarbeiter: ~1 StundeSprint Retrospective Meeting: Reflektion des letzten Sprints.Ziel: Verbesserungspotential f&uuml;r die zuk&uuml;nftigen Sprints zu finden. Interne Projektprozesse und Spielregeln kritisch zu betrachten.Ablauf: Am Ende eines Sprints setzt sich das Team zusammen und bespricht was im vergangenem Sprint gut bzw. schlecht gelaufen ist.Aufzeichnungen: Ein Protokoll wird erstellt.Dauer: ~1/2h'
+	},{
 		name: 'fp',
-		text: 'In der Regel sollte nach einem abgeschlossenen Sprint Review Meeting ein funktionierendes Produkt bereitstehen. Der Auftraggeber kann danach entscheiden, ob er noch weitere Features in das Produkt implemeniteren lassen will oder ob er
-			   das bereits vorhandene Produkt nimmt und damit das Projekt abschließt.'
-	];
+		text: 'In der Regel sollte nach einem abgeschlossenen Sprint Review Meeting ein funktionierendes Produkt bereitstehen. Der Auftraggeber kann danach entscheiden, ob er noch weitere Features in das Produkt implemeniteren lassen will oder ob er das bereits vorhandene Produkt nimmt und damit das Projekt abschließt.'
+	}];
 }]);
